c0426a2daa065af5110e8106fd0fddbb9f0268fc
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-18 17:07:59 +0100

    define default svn credentials in build config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,11 @@
 grails.plugin.repos.discovery.pillarone = "https://svn.intuitive-collaboration.com/GrailsPlugins/"
 grails.plugin.repos.distribution.pillarone = "https://svn.intuitive-collaboration.com/GrailsPlugins/"
 
+user.svn.username.discovery.pillarone = ""
+user.svn.password.discovery.pillarone = ""
+user.svn.username.distribution.pillarone = ""
+user.svn.password.distribution.pillarone = ""
+
 grails.plugin.repos.resolveOrder = ['pillarone', 'default', 'core']
 
 coverage {

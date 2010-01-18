08f99a2201028a780eac3d4d5dd0f632f8471c05
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-18 17:21:44 +0100

    changed buildconfig again

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,10 +2,6 @@ grails.plugin.repos.discovery.pillarone = "https://svn.intuitive-collaboration.c
 user.svn.username.discovery.pillarone = ""
 user.svn.password.discovery.pillarone = ""
 
-//If publishing this plugin, uncomment this line and provide credentials in ~/.grails/settings.groovy
-//grails.plugin.repos.distribution.pillarone = "https://svn.intuitive-collaboration.com/GrailsPlugins/"
-
-
 grails.plugin.repos.resolveOrder = ['pillarone', 'default', 'core']
 
 coverage {

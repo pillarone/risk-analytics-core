8fcef655e69179fea08e810ee9d22bb7e7b44cfb
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-22 13:40:50 +0100

    new plugin version

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.core.output.batch.GenericBulkInsert
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "0.4.5.1"
+    def version = "0.4.5.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on

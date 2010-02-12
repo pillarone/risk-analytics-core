81fdd9a985303a18515b6ca88590a5996753f30b
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-02-12 15:52:16 +0100

    fixed typo in exception handling

--- src/groovy/org/pillarone/riskanalytics/core/simulation/item/Parameterization.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/simulation/item/Parameterization.groovy
@@ -56,7 +56,7 @@ class Parameterization extends ModellingItem {
     }
 
     //TODO (msp): enable again, refactoring is required because we cannot reference Business logic here (DistributionType)
-    /** @return list of validation errors                      */
+    /** @return list of validation errors                       */
     /*List validate() {
         // dk: there may be more than one validation service in the future.
         // For the moment, we only validate parameter of distribution types
@@ -176,7 +176,7 @@ class Parameterization extends ModellingItem {
             if (getModelClass() != null)
                 eq('modelClassName', getModelClass().name)
         }
-        return results.size() > 0 ? results.get(0): null
+        return results.size() > 0 ? results.get(0) : null
     }
 
 
@@ -226,7 +226,7 @@ class Parameterization extends ModellingItem {
         try {
             result = SimulationRun.findByParameterizationAndToBeDeleted(dao, false)
         } catch (Exception e) {
-            LOG.error "Exception error in method isUsedInSimulation : $ex"
+            LOG.error("Exception in method isUsedInSimulation : $e.message", e)
         }
         result != null
     }

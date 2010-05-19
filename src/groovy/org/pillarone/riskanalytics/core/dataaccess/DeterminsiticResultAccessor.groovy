dd8b679b9788cb34f88696b6bce20802f21137cc
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2010-05-19 18:07:16 +0200

    iteration default value 1 instead of 0

--- src/groovy/org/pillarone/riskanalytics/core/dataaccess/DeterminsiticResultAccessor.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/dataaccess/DeterminsiticResultAccessor.groovy
@@ -6,23 +6,23 @@ import org.pillarone.riskanalytics.core.output.SymbolicValueResult
 public class DeterminsiticResultAccessor {
 
 
-    static Double getSingleValueFromView(SimulationRun simulationRun, String fieldName, String collectorName, String pathName, int periodIndex, int iteration = 0) {
+    static Double getSingleValueFromView(SimulationRun simulationRun, String fieldName, String collectorName, String pathName, int periodIndex, int iteration = 1) {
         def value = SymbolicValueResult.executeQuery("SELECT value FROM ${SymbolicValueResult.name} " +
-            "WHERE iteration = ? AND path = ? AND " +
-            "collector = ? AND " +
-            "field = ? AND " +
-            "period = ? AND " +
-            "simulation_run_id = ?", [iteration, pathName, collectorName, fieldName, periodIndex, simulationRun.id])
+                "WHERE iteration = ? AND path = ? AND " +
+                "collector = ? AND " +
+                "field = ? AND " +
+                "period = ? AND " +
+                "simulation_run_id = ?", [iteration, pathName, collectorName, fieldName, periodIndex, simulationRun.id])
 
         return value[0]
     }
 
-    static List getAllPeriodValuesFromView(SimulationRun simulationRun, String fieldName, String collectorName, String pathName, int iteration = 0) {
+    static List getAllPeriodValuesFromView(SimulationRun simulationRun, String fieldName, String collectorName, String pathName, int iteration = 1) {
         def value = SymbolicValueResult.executeQuery("SELECT value FROM ${SymbolicValueResult.name} " +
-            "WHERE iteration = ? AND path = ? AND " +
-            "collector = ? AND " +
-            "field = ? AND " +
-            "simulation_run_id = ? order by period", [iteration, pathName, collectorName, fieldName, simulationRun.id])
+                "WHERE iteration = ? AND path = ? AND " +
+                "collector = ? AND " +
+                "field = ? AND " +
+                "simulation_run_id = ? order by period", [iteration, pathName, collectorName, fieldName, simulationRun.id])
 
         return value
     }

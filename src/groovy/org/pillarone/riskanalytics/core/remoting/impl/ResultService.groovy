2cfe35f392d236602d70ff3d60f85960a0d8f77f
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2010-12-06 16:15:28 +0100

    ART-188: Allow for wildcards in the result paths  - using regEx  (.*) to select results

--- src/groovy/org/pillarone/riskanalytics/core/remoting/impl/ResultService.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/remoting/impl/ResultService.groovy
@@ -1,15 +1,16 @@
 package org.pillarone.riskanalytics.core.remoting.impl
 
+import java.text.SimpleDateFormat
 import org.pillarone.riskanalytics.core.ParameterizationDAO
-import org.pillarone.riskanalytics.core.output.SimulationRun
-import org.pillarone.riskanalytics.core.remoting.IResultService
-import org.pillarone.riskanalytics.core.remoting.ResultInfo
-import org.pillarone.riskanalytics.core.remoting.SimulationInfo
 import org.pillarone.riskanalytics.core.dataaccess.ResultAccessor
 import org.pillarone.riskanalytics.core.output.AggregatedCollectingModeStrategy
-import java.text.SimpleDateFormat
-import org.pillarone.riskanalytics.core.simulation.item.Parameterization
+import org.pillarone.riskanalytics.core.output.PostSimulationCalculation
+import org.pillarone.riskanalytics.core.output.SimulationRun
+import org.pillarone.riskanalytics.core.remoting.IResultService
 import org.pillarone.riskanalytics.core.remoting.ParameterizationInfo
+import org.pillarone.riskanalytics.core.remoting.ResultInfo
+import org.pillarone.riskanalytics.core.remoting.SimulationInfo
+import org.pillarone.riskanalytics.core.simulation.item.Parameterization
 
 class ResultService implements IResultService {
 
@@ -38,7 +39,7 @@ class ResultService implements IResultService {
             //period label aren't dates
         }
         for (int i = 0; i < run.periodCount; i++) {
-            for (String fullPath in paths) {
+            for (String fullPath in getPathsOfResult(paths, run)) {
                 ResultInfo info = new ResultInfo(path: fullPath, periodIndex: i)
                 if (i < periodDates.size()) {
                     info.periodDate = periodDates[i]
@@ -58,6 +59,40 @@ class ResultService implements IResultService {
         }
         return result
 
+    }
+
+    /**
+     * @param paths
+     * @return paths where with regEx is replaced with existing subcomponents in a specific result
+     */
+    private List<String> getPathsOfResult(List<String> paths, SimulationRun run) {
+        if (!regExIsUsed(paths)) return paths
+        Set<String> fullPaths = new HashSet<String>()
+        List<String> allPaths = PostSimulationCalculation.executeQuery("SELECT  path.pathName FROM ${PostSimulationCalculation.class.name} as p  WHERE p.run.id = ? ", [run.id])
+        for (int i = 0; i < paths.size(); i++) {
+            String field = paths[i].substring(paths[i].lastIndexOf(":") + 1)
+            String searchedPath = paths[i].substring(0, paths[i].lastIndexOf(":"))
+            if (searchedPath.indexOf("(.*)") != -1) {
+                for (int j = 0; j < allPaths.size(); j++) {
+                    String path = allPaths[j]
+                    def matcher = path =~ searchedPath
+                    if (matcher != null && matcher.size() > 0) {
+                        fullPaths << matcher[0][0] + ":" + field
+                    }
+                }
+            } else
+                fullPaths << paths[i]
+        }
+        return fullPaths as List<String>;
+    }
+
+    boolean regExIsUsed(List<String> paths) {
+        for (String searchedPath: paths) {
+            if (searchedPath.indexOf("(.*)") != -1) {
+                return true
+            }
+        }
+        return false
     }
 
     List<SimulationInfo> getSimulationInfos(long parameterizationId) {

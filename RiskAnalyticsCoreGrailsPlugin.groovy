41ec997573f557c38ede448bf6ec27a5641b100f
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-02-15 09:51:25 +0100

    trigger plugin release

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -17,7 +17,7 @@ import org.springframework.transaction.interceptor.TransactionProxyFactoryBean
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.3-ALPHA-2.2"
+    def version = "1.3-ALPHA-2.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on

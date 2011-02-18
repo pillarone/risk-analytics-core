e1e5f03796a0c7a3b0b94ce23ebfe9ae4a333f99
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-02-18 08:29:08 +0100

    releasing plugin version 1.2.1

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -10,7 +10,7 @@ import org.pillarone.riskanalytics.core.output.SingleValueCollectingModeStrategy
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.2.0.1"
+    def version = "1.2.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on

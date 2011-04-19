36c4708aa6f79e0cd7f97c732abeb0888c019301
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-04-19 10:36:59 +0200

    better way to load old parameterizations in migration test cases

--- test/integration/org/pillarone/riskanalytics/core/model/migration/ModelMigratorTests.groovy
+++ test/integration/org/pillarone/riskanalytics/core/model/migration/ModelMigratorTests.groovy
@@ -1,38 +1,64 @@
 package org.pillarone.riskanalytics.core.model.migration
 
 import models.migratableCore.MigratableCoreModel
-import org.pillarone.riskanalytics.core.example.parameter.ExampleParameterObjectClassifier
+import models.migratableCore.MigratableCoreModel.Migration_v1_v2
 import org.pillarone.riskanalytics.core.fileimport.ModelFileImportService
 import org.pillarone.riskanalytics.core.fileimport.ModelStructureImportService
 import org.pillarone.riskanalytics.core.fileimport.ResultConfigurationImportService
+import org.pillarone.riskanalytics.core.parameterization.ParameterizationHelper
 import org.pillarone.riskanalytics.core.simulation.item.Parameterization
-import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterHolderFactory
-import org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType
-import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterObjectParameterHolder
-import org.pillarone.riskanalytics.core.example.migration.ResultViewMode
 import org.pillarone.riskanalytics.core.simulation.item.parameter.MultiDimensionalParameterHolder
+import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterObjectParameterHolder
 
 class ModelMigratorTests extends GroovyTestCase {
+
+    String oldParameterization = """
+package models.migratableCore
+
+model=models.migratableCore.MigratableCoreModel
+periodCount=1
+applicationVersion='1.3'
+periodLabels=["2011-03-30","2011-03-30","2011-03-30"]
+components {
+	composite {
+		parmStrategy[0]=org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.getStrategy(org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.THREE_COLUMNS, ["table":new org.pillarone.riskanalytics.core.parameterization.ConstrainedMultiDimensionalParameter(org.pillarone.riskanalytics.core.util.GroovyUtils.toList([["S1", "S2"], ["Motor", "Engine"], [100.0, 80.0]]),["id","type","value"], org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory.getConstraints('constrained table')),"mode":org.pillarone.riskanalytics.core.example.migration.ResultViewMode.INCREMENTAL,])
+		parmTimeMode[0]=org.pillarone.riskanalytics.core.example.migration.TimeMode.PERIOD
+	}
+	dynamic {
+		subOpt1 {
+			parmStrategy[0]=org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.getStrategy(org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.THREE_COLUMNS, ["table":new org.pillarone.riskanalytics.core.parameterization.ConstrainedMultiDimensionalParameter(org.pillarone.riskanalytics.core.util.GroovyUtils.toList([["S1", "S2"], ["Motor", "Engine"], [100.0, 80.0]]),["id","type","value"], org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory.getConstraints('constrained table')), "mode": org.pillarone.riskanalytics.core.example.migration.ResultViewMode.INCREMENTAL,])
+			parmTimeMode[0]=org.pillarone.riskanalytics.core.example.migration.TimeMode.PERIOD
+		}
+	}
+	exampleInputOutputComponent {
+		parmNewParameterObject[0]=org.pillarone.riskanalytics.core.example.parameter.ExampleParameterObjectClassifier.getStrategy(org.pillarone.riskanalytics.core.example.parameter.ExampleParameterObjectClassifier.TYPE0, ["b":1.0,"a":0.0,])
+		parmParameterObject[0]=org.pillarone.riskanalytics.core.example.parameter.ExampleParameterObjectClassifier.getStrategy(org.pillarone.riskanalytics.core.example.parameter.ExampleParameterObjectClassifier.TYPE0, ["b":1.0,"a":0.0,])
+	}
+}
+comments=[]
+
+    """
 
     void setUp() {
         new ModelFileImportService().compareFilesAndWriteToDB(["MigratableCore"])
         new ModelStructureImportService().compareFilesAndWriteToDB(["MigratableCore"])
         new ResultConfigurationImportService().compareFilesAndWriteToDB(["MigratableCore"])
 
-        //fake an old parameterization
-        Parameterization old = new Parameterization("MigratableCoreParams")
-        old.periodCount = 1
-        old.modelClass = MigratableCoreModel
-        ParameterObjectParameterHolder holder = ParameterHolderFactory.getHolder("composite:parmStrategy", 0, TestConstraintsTableType.getStrategy(org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.THREE_COLUMNS, ["table": new org.pillarone.riskanalytics.core.parameterization.ConstrainedMultiDimensionalParameter(org.pillarone.riskanalytics.core.util.GroovyUtils.toList([["S1", "S2"], ["Motor", "Engine"], [100.0, 80.0]]), ["id", "type", "value"], org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory.getConstraints('constrained table')), "mode": org.pillarone.riskanalytics.core.example.migration.ResultViewMode.INCREMENTAL,]))
-        holder.classifierParameters.put("mode", ParameterHolderFactory.getHolder("${holder.path}:mode", 0, ResultViewMode.INCREMENTAL))
-        old.addParameter(holder)
-        old.addParameter(ParameterHolderFactory.getHolder("composite:parmTimeMode", 0, org.pillarone.riskanalytics.core.example.migration.TimeMode.PERIOD))
-        old.addParameter(ParameterHolderFactory.getHolder("dynamic:subOpt1:parmStrategy", 0, TestConstraintsTableType.getStrategy(org.pillarone.riskanalytics.core.example.migration.TestConstraintsTableType.THREE_COLUMNS, ["table": new org.pillarone.riskanalytics.core.parameterization.ConstrainedMultiDimensionalParameter(org.pillarone.riskanalytics.core.util.GroovyUtils.toList([["S1", "S2"], ["Motor", "Engine"], [100.0, 80.0]]), ["id", "type", "value"], org.pillarone.riskanalytics.core.parameterization.ConstraintsFactory.getConstraints('constrained table')), "mode": org.pillarone.riskanalytics.core.example.migration.ResultViewMode.INCREMENTAL,])))
-        old.addParameter(ParameterHolderFactory.getHolder("dynamic:subOpt1:parmTimeMode", 0, org.pillarone.riskanalytics.core.example.migration.TimeMode.PERIOD))
-        old.addParameter(ParameterHolderFactory.getHolder("exampleInputOutputComponent:parmParameterObject", 0, ExampleParameterObjectClassifier.TYPE0.getParameterObject(["a": 0d, "b": 1d])))
-        old.addParameter(ParameterHolderFactory.getHolder("exampleInputOutputComponent:parmNewParameterObject", 0, ExampleParameterObjectClassifier.TYPE0.getParameterObject(["a": 0d, "b": 1d])))
+        ConfigSlurper slurper = new ConfigSlurper()
 
-        old.save()
+        ModelMigrationClassLoader modelMigrationClassLoader = new ModelMigrationClassLoader([new Migration_v1_v2().oldModelJarURL] as URL[], Thread.currentThread().contextClassLoader)
+        modelMigrationClassLoader.loadClass("models.migratableCore.MigratableCoreModel")
+
+        slurper.classLoader = new GroovyClassLoader(modelMigrationClassLoader)
+        Script script = slurper.classLoader.parseClass(oldParameterization).newInstance()
+
+        try {
+            ConfigObject configObject = slurper.parse(script)
+            Parameterization parameterization = ParameterizationHelper.createParameterizationFromConfigObject(configObject, "MigratableCoreParams")
+            parameterization.save()
+        } finally {
+            GroovySystem.metaClassRegistry.removeMetaClass(script.class)
+        }
     }
 
     void testOldModel() {

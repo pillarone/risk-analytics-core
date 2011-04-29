8629daada17c7e51d813a694a0d28511724658c8
Fouad Jaada <fouad.jaada@intuitive-collaboration.com>
2011-04-29 16:36:00 +0200

    delete all comments, batch by DB cleaning

--- grails-app/services/org/pillarone/riskanalytics/core/output/DBCleanUpService.groovy
+++ grails-app/services/org/pillarone/riskanalytics/core/output/DBCleanUpService.groovy
@@ -1,8 +1,10 @@
 package org.pillarone.riskanalytics.core.output
 
-import org.pillarone.riskanalytics.core.output.PostSimulationCalculation
-import org.pillarone.riskanalytics.core.output.SimulationRun
-import org.pillarone.riskanalytics.core.output.SingleValueResult
+import org.pillarone.riskanalytics.core.BatchRun
+import org.pillarone.riskanalytics.core.BatchRunSimulationRun
+import org.pillarone.riskanalytics.core.parameter.comment.CommentTag
+import org.pillarone.riskanalytics.core.parameter.comment.ParameterizationCommentDAO
+import org.pillarone.riskanalytics.core.parameter.comment.ResultCommentDAO
 
 class DBCleanUpService {
 
@@ -10,11 +12,27 @@ class DBCleanUpService {
 
     def cleanUp() {
         println "starting db cleanup service."
+
+        println "# comments before cleanup count : CommentTag = ${CommentTag.count()} ParameterizationCommentDAO = ${ParameterizationCommentDAO.count()} ResultCommentDAO = ${ResultCommentDAO.count()}"
+        CommentTag.executeUpdate("delete ${CommentTag.name}".toString())
+        ParameterizationCommentDAO.executeUpdate("delete ${ParameterizationCommentDAO.name}".toString())
+        ResultCommentDAO.executeUpdate("delete ${ResultCommentDAO.name}".toString())
+        println "# comments after cleanup count : CommentTag = ${CommentTag.count()} ParameterizationCommentDAO = ${ParameterizationCommentDAO.count()} ResultCommentDAO = ${ResultCommentDAO.count()}"
+
+        println "# BatchRunSimulationRun before cleanup: ${BatchRunSimulationRun.count()}"
+        BatchRunSimulationRun.executeUpdate("delete ${BatchRunSimulationRun.name}".toString())
+        println "# BatchRunSimulationRun after cleanup: ${BatchRunSimulationRun.count()}"
+
         println "# simulations before cleanup: ${SimulationRun.count()}"
         SingleValueResult.executeUpdate("delete ${SingleValueResult.name}".toString())
         PostSimulationCalculation.executeUpdate("delete ${PostSimulationCalculation.name}".toString())
         SimulationRun.executeUpdate("delete ${SimulationRun.name}".toString())
         println "# simulations after cleanup: ${SimulationRun.count()}"
+
+        println "# BatchRun before cleanup: ${BatchRun.count()}"
+        BatchRun.executeUpdate("delete ${BatchRun.name}".toString())
+        println "# BatchRun after cleanup: ${BatchRun.count()}"
+
         println "db cleanup service finished."
     }
 }

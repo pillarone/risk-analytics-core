a16c68a9e51417e84645e9bfe86a2ad0b8f13147
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-06-16 12:40:12 +0200

    reformatted queries and fixed null values

--- src/groovy/org/pillarone/riskanalytics/core/output/batch/results/OracleBulkInsert.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/output/batch/results/OracleBulkInsert.groovy
@@ -13,9 +13,15 @@ class OracleBulkInsert extends AbstractResultsBulkInsert {
         String directoryName = "simulation_${simulationRun.id}"
         String externalTableName = "temp_result_${simulationRun.id}"
         try {
-            String createDirectory = "create or replace directory ${directoryName} as '${tempFile.getParent()}'"
-            String createExternalTable = "create table ${externalTableName} ( run_id number(19), period number(19), iteration number(19), path_id number(19), field_id number(19), collector_id number(19), value float(126), value_index number(19), datetime number(19) ) organization external ( default directory ${directoryName} access parameters ( records delimited by newline fields terminated by ',' ) location ('${tempFile.name}') )"
-            String insert = "insert into single_value_result (id, version, simulation_run_id, period, iteration, path_id, collector_id, field_id, value_index, value, date_time) (select result_id_sequence.nextval, 0, run_id, period, iteration, path_id, collector_id, field_id, value_index, value, datetime from ${externalTableName})"
+            String createDirectory = "CREATE OR REPLACE DIRECTORY ${directoryName} as '${tempFile.getParent()}'"
+            String createExternalTable = "CREATE TABLE ${externalTableName} ( " +
+                    "run_id NUMBER(19), period NUMBER(19), iteration NUMBER(19), path_id NUMBER(19), " +
+                    "field_id NUMBER(19), collector_id NUMBER(19), value FLOAT(126), value_index NUMBER(19), " +
+                    "datetime NUMBER(19) ) ORGANIZATION EXTERNAL ( DEFAULT DIRECTORY ${directoryName} " +
+                    "ACCESS PARAMETERS ( RECORDS DELIMITED BY newline FIELDS TERMINATED BY ',' ) LOCATION ('${tempFile.name}') )"
+            String insert = "INSERT INTO single_value_result " +
+                    "(id, version, simulation_run_id, period, iteration, path_id, collector_id, field_id, value_index, value, date_time) " +
+                    "(SELECT result_id_sequence.nextval, 0, run_id, period, iteration, path_id, collector_id, field_id, value_index, value, datetime FROM ${externalTableName})"
             sql.execute(createDirectory)
             sql.execute(createExternalTable)
             int numberOfResults = sql.executeUpdate(insert)
@@ -34,8 +40,7 @@ class OracleBulkInsert extends AbstractResultsBulkInsert {
 
     @Override
     protected void writeResult(List values) {
-        //TODO: properly handle null dates
-        writer.append(values.collect { it == null ? 0 : it}.join(","))
+        writer.append(values.collect { it == null ? "" : it}.join(","))
         writer.newLine()
     }
 

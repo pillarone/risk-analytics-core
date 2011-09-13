1a744a5c8cd4ed6ffe9e1737cb2547ed65e59f4b
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-09-13 07:30:41 +0200

    releasing plugin version 1.4-BETA-4

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -24,7 +24,7 @@ import org.pillarone.riskanalytics.core.output.aggregation.SumAggregatorSingleVa
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.4-BETA-3.2"
+    def version = "1.4-BETA-4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on

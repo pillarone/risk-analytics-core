0104b993cefa5444312b7b62c271695fbab300b6
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-10-04 16:16:40 +0200

    Merge branch 'master' of /home/michi/dev/RiskAnalytics/RiskAnalyticsCore with conflicts.

---
+++ scripts/_Events.groovy
@@ -0,0 +1,46 @@
+import net.sf.jasperreports.engine.JasperCompileManager
+import grails.util.BuildSettingsHolder
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+
+eventCompileEnd = {
+    println "[jasper] - compiling reports"
+    for (File reportFolder in reportFolders) {
+        if (reportFolder.exists()) {
+            reportFolder.eachFileRecurse groovy.io.FileType.FILES, { File sourceFile ->
+                if (sourceFile.name.endsWith(".jrxml")) {
+                    File jasperFile = new File(sourceFile.absolutePath.replace(".jrxml", ".jasper"))
+                    if (!jasperFile.exists() || jasperFile.lastModified() < sourceFile.lastModified()) {
+                        println "[jasper] - compiling ${sourceFile.name}"
+                        JasperCompileManager.compileReportToStream(sourceFile.newInputStream(), jasperFile.newOutputStream())
+                    }
+                }
+            }
+        } else {
+            println "[jasper] - no reports found in ${reportFolder}"
+        }
+    }
+}
+
+eventCleanEnd = {
+    println "[jasper] - deleting reports"
+    for (File reportFolder in reportFolders) {
+        if (reportFolder.exists()) {
+            reportFolder.eachFileRecurse groovy.io.FileType.FILES, { File file ->
+                if (file.name.endsWith(".jasper")) {
+                    println "[jasper] - deleting ${file.name}"
+                    file.delete()
+                }
+            }
+        }
+    }
+}
+
+private List<File> getReportFolders() {
+    final def folders = buildConfig.reportFolders
+    if (folders instanceof List) {
+        return folders
+    }
+
+    return []
+}

18028e85b6a4ef5daae9d537212c6bcbe0b0713d
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-02-03 15:45:28 +0100

    releasing new plugin version 1.5-ALPHA-2.1-kti

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -29,7 +29,7 @@ import org.pillarone.riskanalytics.core.listener.ModellingItemHibernateListener
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.5-ALPHA-2-kti"
+    def version = "1.5-ALPHA-2.1-kti"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on

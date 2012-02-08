3e9b488ebb3d1268840fbd55c0a42b814a55c143
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2012-02-08 13:46:14 +0100

    ART-245: improved RPH structure

--- src/groovy/org/pillarone/riskanalytics/core/simulation/item/parameter/ResourceParameterHolder.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/simulation/item/parameter/ResourceParameterHolder.groovy
@@ -3,10 +3,14 @@ package org.pillarone.riskanalytics.core.simulation.item.parameter
 import org.pillarone.riskanalytics.core.parameter.Parameter
 import org.pillarone.riskanalytics.core.parameter.ResourceParameter
 import org.pillarone.riskanalytics.core.simulation.item.Resource
+import org.pillarone.riskanalytics.core.simulation.item.VersionNumber
 
 class ResourceParameterHolder extends ParameterHolder {
 
-    Resource resource
+    String name
+    String version
+    Class resourceClass
+
 
     ResourceParameterHolder(Parameter parameter) {
         super(parameter)
@@ -14,36 +18,57 @@ class ResourceParameterHolder extends ParameterHolder {
 
     ResourceParameterHolder(String path, int periodIndex, Resource resource) {
         super(path, periodIndex)
-        this.resource = resource
+        name = resource.name
+        version = resource.versionNumber.toString()
+        resourceClass = resource.modelClass
     }
 
     @Override
     void applyToDomainObject(Parameter parameter) {
-        parameter = parameter as ResourceParameter
-        parameter.name = resource.name
-        parameter.itemVersion = resource.versionNumber.toString()
-        parameter.resourceClassName = resource.modelClass.name
+        parameter.name = name
+        parameter.itemVersion = version
+        parameter.resourceClassName = resourceClass.name
     }
 
     @Override
     void setParameter(Parameter parameter) {
-        parameter = parameter as ResourceParameter
-        resource = new Resource(parameter.name, Thread.currentThread().contextClassLoader.loadClass(parameter.resourceClassName))
-        resource.load(false)
+        name = parameter.name
+        resourceClass = Thread.currentThread().contextClassLoader.loadClass(parameter.resourceClassName)
+        version = parameter.itemVersion
     }
 
     @Override
     Object getBusinessObject() {
+        Resource resource = new Resource(name,resourceClass)
+        resource.versionNumber = new VersionNumber(version)
         return resource.resourceInstance
     }
 
     @Override
     protected void updateValue(Object newValue) {
-
+        NameVersionPair pair = newValue as NameVersionPair
+        name = pair.name
+        version = pair.version
     }
 
     @Override
     Parameter createEmptyParameter() {
         return new ResourceParameter(path: path, periodIndex: periodIndex)
+    }
+
+    public static class NameVersionPair {
+        String name
+        String version
+
+        NameVersionPair(String name, String version) {
+            this.name = name
+            this.version = version
+        }
+
+        @Override
+        String toString() {
+            return "${name} v${version}"
+        }
+
     }
 }

a3aaf4432717110fc90deb1b0084e76bc067c8ea
bzetterstrom <bjorn.zetterstrom@art-allianz.com>
2012-03-20 15:48:58 +0100

    added capability to set a user name to run as a user in stand alone mode by adding the flag -DrunAsUserName=<username>

--- src/groovy/org/pillarone/riskanalytics/core/user/UserManagement.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/user/UserManagement.groovy
@@ -9,20 +9,34 @@ class UserManagement {
     public static final String USER_ROLE = "ROLE_USER"
     public static final String REVIEWER_ROLE = "ROLE_REVIEWER"
     public static final String ADMIN_ROLE = "ROLE_ADMIN"
+    static boolean _hasTriedFindingFixUserNameFromSystemProperty = false
+    static Person _possiblyFixUser
 
 
     public static Person getCurrentUser() {
         SpringSecurityService securityService = getSpringSecurityService()
         GrailsUser user = securityService.getPrincipal()
-        return user != null ? Person.get(user.id) : null
+        return user != null ? Person.get(user.id) : tryFindingFixUserNameFromSystemProperty()
     }
 
+    private static Person tryFindingFixUserNameFromSystemProperty() {
+        if (_hasTriedFindingFixUserNameFromSystemProperty) {
+            return _possiblyFixUser
+        }
+        Person.withTransaction {e ->
+            String runAsUserName = System.getProperty("runAsUserName");
+            _possiblyFixUser = runAsUserName != null ? Person.findByUsername(runAsUserName) : null
+            if (_possiblyFixUser) {
+                // loading the settings seems needed here for later retrieval...?
+                _possiblyFixUser.settings
+            }
+            _hasTriedFindingFixUserNameFromSystemProperty = true
+            return _possiblyFixUser
+        }
+    }
+    
     static SpringSecurityService getSpringSecurityService() {
         SpringSecurityService securityService = ApplicationHolder.application.mainContext.getBean("springSecurityService")
         return securityService
-    }
-
-    public static boolean isLoggedIn() {
-        return getSpringSecurityService().isLoggedIn()
     }
 }

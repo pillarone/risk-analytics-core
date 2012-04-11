3066b66345103be41a90ee7b01d17e48063aced7
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-04-11 16:10:24 +0200

    releasing new plugin version 1.5-ALPHA-4.3

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -29,7 +29,7 @@ import org.pillarone.riskanalytics.core.simulation.engine.MappingCache
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.5-ALPHA-4.2"
+    def version = "1.5-ALPHA-4.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on

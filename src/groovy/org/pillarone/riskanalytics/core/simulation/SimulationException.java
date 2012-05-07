89eafac7ae32e4d7f25c07afbc6036c32b4c7d50
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-05-07 17:41:09 +0200

    ART-810: catching SimulationExpception in Component.execute(). Be aware that all exceptions checking the projection horizon have been modified to RuntimeException.

---
+++ src/groovy/org/pillarone/riskanalytics/core/simulation/SimulationException.java
@@ -0,0 +1,48 @@
+package org.pillarone.riskanalytics.core.simulation;
+
+import org.pillarone.riskanalytics.core.components.ComponentUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simulation exception extends a normal runtime exception with the (component) path it occurs in.
+ * The path is extended i.e. by each recursive call of Component.execute(). Manually extension is also possible by using
+ * the method addPathElement(). getMessage() is then enhanced with the path.
+ *
+ * @author stefan.kunz (at) intuitive-collaboration (dot) com
+ */
+public class SimulationException extends RuntimeException {
+
+    private List<String> pathElements = new ArrayList<String>();
+
+    public SimulationException(String message) {
+        super(message);
+    }
+
+    public void addPathElement(String pathElement) {
+        // always insert at position 0 to get the path in the correct order
+        pathElements.add(0, pathElement);
+    }
+
+    String path() {
+        StringBuilder technicalPath = new StringBuilder();
+        boolean firstPassed = false;
+        for (String pathElement : pathElements) {
+            if (firstPassed) {
+                technicalPath.append(ComponentUtils.PATH_SEPARATOR);
+            }
+            else {
+                firstPassed = true;
+            }
+            technicalPath.append(ComponentUtils.getNormalizedName(pathElement));
+        }
+        return technicalPath.toString();
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + "\nPath: " + path();
+    }
+
+}

cc3af489d257be3d711c118b639df9e70d860a9f
Simon Parten <simon.parten@art-allianz.com>
2012-08-10 14:37:34 +0200

    Jenkins paused halfway through. Commit a line break and have him retry the build.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,6 +9,7 @@ grails.project.dependency.resolution = {
         grailsHome()
         grailsCentral()
         mavenCentral()
+
     }
 
     //even though this plugin does not need anything from this repo, it has to be added for the deploy script to check existing plugins

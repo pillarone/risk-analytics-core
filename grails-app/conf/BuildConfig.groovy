e75a3eed1b37d25e0c67592253f6a92627472f02
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2012-12-10 16:41:50 +0100

    missing dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,9 +32,7 @@ grails.project.dependency.resolution = {
             exclude "xml-apis"
         }
         compile('org.apache.poi:poi:3.7')
-        compile('org.apache.poi:poi-ooxml:3.7') {
-            excludes 'xmlbeans'
-        }
+        compile('org.apache.poi:poi-ooxml:3.7')
     }
 }
 

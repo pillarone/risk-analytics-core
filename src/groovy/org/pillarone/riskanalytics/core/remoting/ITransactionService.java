bfbe45c6093f4239b1fcae5945ae780879a147bf
olofandersson <olof.andersson@art-allianz.com>
2013-01-15 08:45:22 +0100

    ART-1065 : Extend TransactionService to provide external transaction url

--- src/groovy/org/pillarone/riskanalytics/core/remoting/ITransactionService.java
+++ src/groovy/org/pillarone/riskanalytics/core/remoting/ITransactionService.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public interface ITransactionService {
 
+    /**
+     * Get all Transactions
+     * @return
+     */
     List<TransactionInfo> getAllTransactions();
+
+    /**
+     * Get all transactions that can be linked from P1
+     * @return
+     */
+    List<TransactionInfo> getLinkableTransactions();
+
+
 
 }

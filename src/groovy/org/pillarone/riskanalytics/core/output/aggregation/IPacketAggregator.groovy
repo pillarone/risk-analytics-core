770ba94c346bd7d1f69907ea5d740f32a92d641b
detlef-brendle <detlef.brendle@canoo.com>
2013-01-22 15:14:42 +0100

    change signature using List instead of PacketList on IPacketAggregator

--- src/groovy/org/pillarone/riskanalytics/core/output/aggregation/IPacketAggregator.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/output/aggregation/IPacketAggregator.groovy
@@ -1,10 +1,8 @@
 package org.pillarone.riskanalytics.core.output.aggregation
 
-import org.pillarone.riskanalytics.core.packets.PacketList
 import org.pillarone.riskanalytics.core.packets.Packet
-
 
 interface IPacketAggregator<E extends Packet> {
 
-    E aggregate(PacketList<E> packetList)
+    E aggregate(List<E> packetList)
 }

60a331c39c06cb70212688e4f579a74b9d4e6d56
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2013-03-20 16:43:22 +0100

    ReportUtils does no longer work in groovy

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -33,9 +33,9 @@ import org.springframework.transaction.interceptor.TransactionProxyFactoryBean
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.7-a2"
+    def version = "1.7-a3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
     def dependsOn = [
             "backgroundThread": "1.3",
---
+++ src/groovy/org/pillarone/riskanalytics/core/report/ReportUtils.java
@@ -0,0 +1,83 @@
+package org.pillarone.riskanalytics.core.report;
+
+import org.pillarone.riskanalytics.core.report.impl.ReportDataCollection;
+import org.pillarone.riskanalytics.core.report.impl.ModellingItemReportData;
+import org.pillarone.riskanalytics.core.simulation.item.ModellingItem;
+import org.pillarone.riskanalytics.core.parameterization.ParameterApplicator;
+import org.pillarone.riskanalytics.core.model.Model;
+import org.pillarone.riskanalytics.core.simulation.item.Parameterization;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * bzetterstrom
+ */
+public abstract class ReportUtils {
+    public static ModellingItem getSingleModellingItem(IReportData reportData) {
+        final ModellingItem[] modellingItem = new ModellingItem[1];
+        reportData.accept(new IReportDataVisitor() {
+            public void visitModellingItemReportData(ModellingItemReportData modellingItemReportData) {
+                modellingItem[0] = (ModellingItem) modellingItemReportData.getItem();
+            }
+
+            public void visitReportDataCollection(ReportDataCollection reportDataCollection) {
+                if (reportDataCollection.size() == 0) {
+                    throw new UnsupportedReportParameterException("Please select an item for this report");
+                } else if (reportDataCollection.size() > 1) {
+                    throw new UnsupportedReportParameterException("Please select only one item for this report");
+                } else {
+                    reportDataCollection.get(0).accept(this);
+                }
+            }
+        });
+        return modellingItem[0]; //TODO: ugly
+    }
+
+    public static boolean isSingleItem(final IReportData reportData, final Class expectedItemType) {
+        final boolean[] isSingleItem = new boolean[1];
+        reportData.accept(new IReportDataVisitor() {
+            public void visitModellingItemReportData(ModellingItemReportData modellingItemReportData) {
+                isSingleItem[0] = expectedItemType.isAssignableFrom(modellingItemReportData.getItem().getClass());
+            }
+
+            public void visitReportDataCollection(ReportDataCollection reportDataCollection) {
+                if (reportDataCollection.size() != 1) {
+                    isSingleItem[0] = false;
+                } else {
+                    reportDataCollection.get(0).accept(this);
+                }
+            }
+        });
+        return isSingleItem[0]; //TODO:
+    }
+
+    public static Collection<ModellingItem> getModellingItemCollection(IReportData reportData) {
+        final List<ModellingItem> modellingItems = new ArrayList<ModellingItem>();
+        reportData.accept(new IReportDataVisitor() {
+            public void visitModellingItemReportData(ModellingItemReportData modellingItemReportData) {
+                modellingItems.add((ModellingItem) modellingItemReportData.getItem());
+            }
+
+            public void visitReportDataCollection(ReportDataCollection reportDataCollection) {
+                for (IReportData reportData2 : reportDataCollection) {
+                    reportData2.accept(this);
+                }
+            }
+        });
+        return modellingItems;
+    }
+
+    public static void loadAndApplyParameterizationToModel(Model model, Parameterization parameterization) {
+        model.init();
+
+        ParameterApplicator applicator = new ParameterApplicator();
+        applicator.setModel(model);
+        parameterization.load(true);
+        applicator.setParameterization(parameterization);
+        applicator.init();
+        applicator.applyParameterForPeriod(0);
+    }
+
+}

52b1eb3aab3a395befea54e07261a837f9028730
Simon Parten <simon.parten@art-allianz.com>
2013-04-08 18:20:13 +0200

    Randomly added compile static to sanitise some stack traces.

--- src/groovy/org/pillarone/riskanalytics/core/output/batch/results/GenericBulkInsert.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/output/batch/results/GenericBulkInsert.groovy
@@ -1,11 +1,13 @@
 package org.pillarone.riskanalytics.core.output.batch.results
 
+import groovy.transform.CompileStatic
 import org.pillarone.riskanalytics.core.output.CollectorMapping
 import org.pillarone.riskanalytics.core.output.FieldMapping
 import org.pillarone.riskanalytics.core.output.PathMapping
 import org.pillarone.riskanalytics.core.output.SingleValueResult
 import org.joda.time.DateTime
 
+@CompileStatic
 class GenericBulkInsert extends AbstractResultsBulkInsert {
 
     protected void save() {
@@ -15,9 +17,9 @@ class GenericBulkInsert extends AbstractResultsBulkInsert {
             result.simulationRun = simulationRun
             result.period = Integer.parseInt(values[1])
             result.iteration = Integer.parseInt(values[2])
-            result.path = PathMapping.get(Long.parseLong(values[3]))
-            result.field = FieldMapping.get(Long.parseLong(values[4]))
-            result.collector = CollectorMapping.get(Long.parseLong(values[5]))
+            result.path = (PathMapping) PathMapping.get(Long.parseLong(values[3]))
+            result.field = (FieldMapping) FieldMapping.get(Long.parseLong(values[4]))
+            result.collector = (CollectorMapping) CollectorMapping.get(Long.parseLong(values[5]))
             result.value = Double.parseDouble(values[6])
             result.valueIndex = Integer.parseInt(values[7])
             if (values[8] != "null") {

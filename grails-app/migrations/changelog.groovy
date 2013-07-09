b8db10f305c0af54e6cfc4708794b5a1a3900d46
detlef-brendle <detlef.brendle@canoo.com>
2013-07-09 14:03:01 +0200

    PMO-2531 - File attachments are not linked to p14n version improve changelog script

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -4,8 +4,18 @@ import org.pillarone.riskanalytics.core.parameter.comment.CommentFileDAO
 
 databaseChangeLog = {
     changeSet(author: "detlef.brendle", id: "migrate-comment-files-to-db") {
+        preConditions(onFail: 'MARK_RAN') {
+            grailsPrecondition {
+                check {
+                    if (!(CommentDAO.list().any { it.files })) {
+                        fail('No comment files found in DB.')
+                    }
+                }
+            }
+        }
         grailsChange {
             change {
+                println 'Starting copy comment files into database.'
                 CommentDAO.withNewSession {
                     CommentDAO.list().each { CommentDAO commentDao ->
                         if (commentDao.files) {
@@ -13,9 +23,15 @@ databaseChangeLog = {
                             for (String filename : files) {
                                 File file = new File(FileConstants.COMMENT_FILE_DIRECTORY, filename)
                                 if (file.exists()) {
-                                    commentDao.addToCommentFile(new CommentFileDAO(name: filename, content: file.bytes))
+                                    if (!(commentDao.commentFile.find { it.name == filename })) {
+                                        log.info("Adding file $file.absolutePath to commentDAO")
+                                        commentDao.addToCommentFile(new CommentFileDAO(name: filename, content: file.bytes))
+                                    }
+                                } else {
+                                    log.error("File $file.absolutePath does not exist. Ignore")
                                 }
                             }
+                            commentDao.files = null
                             commentDao.save()
                         }
                     }

b5b67f85b042baef72b370d3d185aaed8ffe472a
detlef-brendle <detlef.brendle@canoo.com>
2013-12-17 14:58:59 +0100

    deployPlugin script for authentication based repository artefact check. (grails jira : GPRELEASE-50)

---
+++ scripts/DeployPlugin.groovy
@@ -0,0 +1,84 @@
+import grails.util.BuildSettingsHolder
+import grails.util.GrailsNameUtils
+
+includeTargets << new File("${releasePluginDir}/scripts/_GrailsMaven.groovy")
+
+
+target(main: "Checks if the plugin is already deployed to maven and deploys if necessary") {
+    depends(parseArguments, packageApp)
+
+    String pluginName
+    String version
+    String groupId
+
+    def pluginInstance = new GroovyClassLoader().loadClass("${appClassName}GrailsPlugin").newInstance()
+
+    RepoDelegate delegate = new RepoDelegate()
+    Closure c = BuildSettingsHolder.settings.config.grails.project.dependency.resolution
+    c.delegate = delegate
+    c.call()
+
+    version = pluginInstance.version
+    if (!version.endsWith('-SNAPSHOT')) {
+
+        try {
+            groupId = pluginInstance.groupId
+        } catch (Exception e) {
+            groupId = "org.grails.plugins"
+        }
+
+        pluginName = GrailsNameUtils.getScriptName(appClassName)
+
+        String artefactPattern = [groupId.replace('.' as char, '/' as char), pluginName, version, pluginName + "-" + version + ".zip"].join('/')
+        for (Repo repo in delegate.repos) {
+            URL url = new URL("${repo.url}$artefactPattern");
+            javax.net.ssl.HttpsURLConnection uc = url.openConnection();
+
+            String userpass = "${repo.user}:${repo.pass}"
+            String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
+
+            uc.setRequestProperty("Authorization", basicAuth);
+            int responseCode = uc.getResponseCode()
+            if (responseCode == 200) {
+                return
+            }
+        }
+    }
+    mavenDeploy()
+
+}
+
+setDefaultTarget(main)
+
+class RepoDelegate {
+
+    List<Repo> repos = []
+
+    void mavenRepo(String s, Closure c) {
+        Repo repo = new Repo(url: s)
+        c.delegate = repo
+        c.call()
+
+        repos << repo
+    }
+
+    void methodMissing(String s, Object o) {
+    }
+
+    void grailsHome() {
+    }
+
+}
+
+class Repo {
+    String url
+    String user
+    String pass
+
+    void auth(def info) {
+        user = info["username"]
+        pass = info["password"]
+    }
+}
+
+

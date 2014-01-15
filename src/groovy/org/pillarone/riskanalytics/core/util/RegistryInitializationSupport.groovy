79aaebb4f003086cebb15d97ae25968989285dbe
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2014-01-15 17:54:18 +0100

    Revert "PMO-2443: auto registration of add on components"
    This reverts commit 4b17aaaa911a58397a8fdaa19771185fa68ae531.

--- src/groovy/org/pillarone/riskanalytics/core/util/RegistryInitializationSupport.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/util/RegistryInitializationSupport.groovy
@@ -7,23 +7,15 @@ class RegistryInitializationSupport {
 
     private static Set<String> packages = new HashSet()
 
-    private static boolean initializing = true
-
     static {
         packages << "org.pillarone"
     }
 
     public static void addBasePackage(String name) {
-        if (initializing) {
-            packages << name
-        } else {
-            throw new IllegalStateException("Cannot add base package ${name} after initialization is finished")
-        }
+        packages << name
     }
 
     public static <E> List<Class<E>> findClasses(Class<E> assignableFrom) {
-        initializing = false
-
         ClassPathScanner scanner = new ClassPathScanner()
         scanner.addIncludeFilter(new AssignableTypeFilter(assignableFrom))
 

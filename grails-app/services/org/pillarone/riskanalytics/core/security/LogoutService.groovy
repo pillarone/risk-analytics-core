13226ac066d55e6416eda6508e9bf6c4ca9649fb
Matthias Ansorge <matthias.ansorge@canoo.com>
2014-02-13 11:43:51 +0100

    PMO-2709: added LogoutService

---
+++ grails-app/services/org/pillarone/riskanalytics/core/security/LogoutService.groovy
@@ -0,0 +1,43 @@
+package org.pillarone.riskanalytics.core.security
+
+import org.codehaus.groovy.grails.web.util.WebUtils
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.LogoutHandler
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class LogoutService {
+
+    List<LogoutHandler> logoutHandlers
+    LogoutSuccessHandler logoutSuccessHandler
+
+    void logout(boolean redirect = false) {
+        Authentication authentication = authentication
+        internalLogout(authentication)
+        if (redirect) {
+            logoutSuccessHandler.onLogoutSuccess(request, response, authentication)
+        }
+
+    }
+
+    private List<LogoutHandler> internalLogout(Authentication authentication) {
+        logoutHandlers.each { LogoutHandler handler ->
+            handler.logout(request, response, authentication)
+        }
+    }
+
+    private HttpServletRequest getRequest() {
+        WebUtils.retrieveGrailsWebRequest().request
+    }
+
+    private HttpServletResponse getResponse() {
+        WebUtils.retrieveGrailsWebRequest().response
+    }
+
+    private Authentication getAuthentication() {
+        SecurityContextHolder.context.authentication
+    }
+}

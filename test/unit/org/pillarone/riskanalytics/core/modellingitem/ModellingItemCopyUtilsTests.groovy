9300287b8c7c522c2850c2abd25f5344f935d75b
Matthias Ansorge <matthias.ansorge@canoo.com>
2014-02-24 15:42:39 +0100

    PMO-2679: modified ModellingItemCopyUtils to not only copy, but also to update ModellingItems

--- test/unit/org/pillarone/riskanalytics/core/modellingitem/ModellingItemCopyUtilsTests.groovy
+++ test/unit/org/pillarone/riskanalytics/core/modellingitem/ModellingItemCopyUtilsTests.groovy
@@ -14,49 +14,82 @@ class ModellingItemCopyUtilsTests {
 
     @Test
     void testCopyP14N() {
+
+        assert !ModellingItemCopyUtils.copyModellingItem(null, new Parameterization('target'))
+
         def creationTime = new DateTime()
         def modTime = new DateTime()
         def creator = new Person()
         def lastUpdater = new Person()
         List tags = [new Tag(name: 'tagName')]
 
-        Parameterization parameterization = new Parameterization('name')
-        parameterization.modelClass = CoreModel
-        parameterization.id = 123l
-        parameterization.versionNumber = new VersionNumber("1.0")
-        parameterization.creationDate = creationTime
-        parameterization.modificationDate = modTime
-        parameterization.creator = creator
-        parameterization.lastUpdater = lastUpdater
-        parameterization.tags = tags
-        parameterization.valid = true
-        parameterization.status = Status.DATA_ENTRY
-        parameterization.dealId = 200l
+        Parameterization source = new Parameterization('name')
+        source.modelClass = CoreModel
+        source.id = 123l
+        source.versionNumber = new VersionNumber("1.0")
+        source.creationDate = creationTime
+        source.modificationDate = modTime
+        source.creator = creator
+        source.lastUpdater = lastUpdater
+        source.tags = tags
+        source.valid = true
+        source.status = Status.DATA_ENTRY
+        source.dealId = 200l
 
-        Parameterization withoutDeps = ModellingItemCopyUtils.copyModellingItem(parameterization, false)
-        assert !withoutDeps.is(parameterization)
-        assert withoutDeps.id == parameterization.id
-        assert withoutDeps.versionNumber == parameterization.versionNumber
-        assert withoutDeps.creationDate == parameterization.creationDate
-        assert withoutDeps.modificationDate == parameterization.modificationDate
-        assert withoutDeps.creator == parameterization.creator
-        assert withoutDeps.lastUpdater == parameterization.lastUpdater
+        Parameterization withoutDeps = ModellingItemCopyUtils.copyModellingItem(source, null, false)
+        assert !withoutDeps.is(source)
+        assert withoutDeps.id == source.id
+        assert withoutDeps.versionNumber == source.versionNumber
+        assert withoutDeps.creationDate == source.creationDate
+        assert withoutDeps.modificationDate == source.modificationDate
+        assert withoutDeps.creator == source.creator
+        assert withoutDeps.lastUpdater == source.lastUpdater
         assert withoutDeps.tags == []
-        assert withoutDeps.valid == parameterization.valid
-        assert withoutDeps.status == parameterization.status
-        assert withoutDeps.dealId == parameterization.dealId
+        assert withoutDeps.valid == source.valid
+        assert withoutDeps.status == source.status
+        assert withoutDeps.dealId == source.dealId
 
-        Parameterization withDeps = ModellingItemCopyUtils.copyModellingItem(parameterization, true)
-        assert !withDeps.is(parameterization)
-        assert withDeps.id == parameterization.id
-        assert withDeps.versionNumber == parameterization.versionNumber
-        assert withDeps.creationDate == parameterization.creationDate
-        assert withDeps.modificationDate == parameterization.modificationDate
-        assert withDeps.creator == parameterization.creator
-        assert withDeps.lastUpdater == parameterization.lastUpdater
-        assert withDeps.tags == parameterization.tags
-        assert withDeps.valid == parameterization.valid
-        assert withDeps.status == parameterization.status
-        assert withDeps.dealId == parameterization.dealId
+        Parameterization withDeps = ModellingItemCopyUtils.copyModellingItem(source, null, true)
+        assert !withDeps.is(source)
+        assert withDeps.id == source.id
+        assert withDeps.versionNumber == source.versionNumber
+        assert withDeps.creationDate == source.creationDate
+        assert withDeps.modificationDate == source.modificationDate
+        assert withDeps.creator == source.creator
+        assert withDeps.lastUpdater == source.lastUpdater
+        assert withDeps.tags == source.tags
+        assert withDeps.valid == source.valid
+        assert withDeps.status == source.status
+        assert withDeps.dealId == source.dealId
+
+        Parameterization target = new Parameterization('target')
+        withoutDeps = ModellingItemCopyUtils.copyModellingItem(source, target, false)
+        assert withoutDeps.is(target)
+        assert !withoutDeps.is(source)
+        assert withoutDeps.id == source.id
+        assert withoutDeps.versionNumber == source.versionNumber
+        assert withoutDeps.creationDate == source.creationDate
+        assert withoutDeps.modificationDate == source.modificationDate
+        assert withoutDeps.creator == source.creator
+        assert withoutDeps.lastUpdater == source.lastUpdater
+        assert withoutDeps.tags == []
+        assert withoutDeps.valid == source.valid
+        assert withoutDeps.status == source.status
+        assert withoutDeps.dealId == source.dealId
+
+        target = new Parameterization('target')
+        withDeps = ModellingItemCopyUtils.copyModellingItem(source, target, true)
+        assert withDeps.is(target)
+        assert !withDeps.is(source)
+        assert withDeps.id == source.id
+        assert withDeps.versionNumber == source.versionNumber
+        assert withDeps.creationDate == source.creationDate
+        assert withDeps.modificationDate == source.modificationDate
+        assert withDeps.creator == source.creator
+        assert withDeps.lastUpdater == source.lastUpdater
+        assert withDeps.tags == source.tags
+        assert withDeps.valid == source.valid
+        assert withDeps.status == source.status
+        assert withDeps.dealId == source.dealId
     }
 }

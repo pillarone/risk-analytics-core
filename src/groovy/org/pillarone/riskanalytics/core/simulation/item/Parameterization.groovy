634cd50e44fb426aa8b1e8d6d1d9588f384a6a68
fazl <fazl.rahman@art-allianz.com>
2014-04-02 18:58:46 +0200

    PMO-2737 log all additions/removals in one line per p14n

--- src/groovy/org/pillarone/riskanalytics/core/simulation/item/Parameterization.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/simulation/item/Parameterization.groovy
@@ -237,6 +237,7 @@ class Parameterization extends ParametrizedItem {
 
     protected void saveTags(ParameterizationDAO dao) {
         List<ParameterizationTag> tagsToRemove = []
+        String logTagNames = "" //PMO-2737
         for (ParameterizationTag tag in dao.tags) {
             if (!tags.contains(tag.tag)) {
                 tagsToRemove << tag
@@ -244,15 +245,18 @@ class Parameterization extends ParametrizedItem {
         }
         for (ParameterizationTag tag in tagsToRemove) {
             dao.removeFromTags(tag)
-            LOG.info("Drop tag: ${tag.tag.name} from P14n: ${getNameAndVersion()}") //PMO-2737
+            logTagNames += "-'${tag.tag.name}',"
         }
         tagsToRemove.each { it.delete() }
 
         for (Tag tag in tags) {
             if (!dao.tags*.tag?.contains(tag)) {
                 dao.addToTags(new ParameterizationTag(tag: tag))
-                LOG.info("Add tag: ${tag.name} to P14n: ${getNameAndVersion()}") //PMO-2737
+                logTagNames += "+'${tag.name}',"
             }
+        }
+        if(logTagNames.length()>0){
+            LOG.info(getNameAndVersion() + " Tags changed:" + logTagNames)
         }
     }
 

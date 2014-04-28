a9878ca44b9156850bbdebcd862ebe196270bfa1
matthiasa <matthias.ansorge@googlemail.com>
2014-04-28 12:40:16 +0200

    fixed DefaultDeleteStrategy

--- src/groovy/org/pillarone/riskanalytics/core/output/DefaultDeleteStrategy.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/output/DefaultDeleteStrategy.groovy
@@ -1,6 +1,5 @@
 package org.pillarone.riskanalytics.core.output
 
-import org.pillarone.riskanalytics.core.BatchRun
 import org.pillarone.riskanalytics.core.simulation.engine.grid.GridHelper
 
 class DefaultDeleteStrategy extends DeleteSimulationStrategy {
@@ -10,13 +9,7 @@ class DefaultDeleteStrategy extends DeleteSimulationStrategy {
         SimulationRun.withTransaction {
             PostSimulationCalculation.findAllByRun(simulationRun)*.delete() // there are only few of them...
             SingleValueResult.executeUpdate("delete from $SingleValueResult.name where simulationRun = ?", [simulationRun])
-            BatchRun batchRun = simulationRun.batchRun
-            if (batchRun) {
-                batchRun.removeFromSimulationRuns(simulationRun)
-                batchRun.save(flush: true)
-            } else {
-                simulationRun.delete(flush: true)
-            }
+            simulationRun.delete(flush: true)
         }
     }
 }

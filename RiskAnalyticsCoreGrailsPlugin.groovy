176a8ad126b35ed0efe24ab3e136ab75715a8578
fazl <fazl.rahman@art-allianz.com>
2014-05-06 17:32:26 +0200

    Use version 1.9.x-SNAPSHOT for ra-apps and ra-core plugins to disambiguate from master branch plugins.

--- RiskAnalyticsCoreGrailsPlugin.groovy
+++ RiskAnalyticsCoreGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.springframework.transaction.interceptor.TransactionProxyFactoryBean
 
 class RiskAnalyticsCoreGrailsPlugin {
     // the plugin version
-    def version = "1.9-SNAPSHOT"
+    def version = "1.9.x-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on

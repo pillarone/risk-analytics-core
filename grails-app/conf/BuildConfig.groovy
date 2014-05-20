a386f7112b7efd22f9fca3be9d9267565f37d4f8
matthiasa <matthias.ansorge@googlemail.com>
2014-05-20 13:29:52 +0200

    ART-1198: upgraded guava to 17.0 (EventBus has ExceptionHandler in this version)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,6 +38,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
+        compile('com.google.guava:guava:17.0')
         runtime 'net.sf.jasperreports:jasperreports:4.0.1', {
             excludes "xml-apis", "commons-collections", "jdtcore"
         }

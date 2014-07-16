0009b839a8e841df2f77e734a43874998232f567
fazl <fazl.rahman@art-allianz.com>
2014-07-16 23:57:30 +0200

    PMO-2837 Add TODO comment

--- src/groovy/org/pillarone/riskanalytics/core/output/DefaultDeleteStrategy.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/output/DefaultDeleteStrategy.groovy
@@ -5,7 +5,7 @@ import org.pillarone.riskanalytics.core.simulation.engine.grid.GridHelper
 class DefaultDeleteStrategy extends DeleteSimulationStrategy {
 
     void deleteSimulation(SimulationRun simulationRun) {
-        new File(GridHelper.getResultLocation(simulationRun.id)).deleteDir()
+        new File(GridHelper.getResultLocation(simulationRun.id)).deleteDir() // TODO PMO-2837 background thread ?
         SimulationRun.withTransaction {
             PostSimulationCalculation.findAllByRun(simulationRun)*.delete() // there are only few of them...
             SingleValueResult.executeUpdate("delete from $SingleValueResult.name where simulationRun = ?", [simulationRun])

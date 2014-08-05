2e59e89b0a1ec5a29f12f4f6fae2dd9f205ad13a
matthiasa <matthias.ansorge@googlemail.com>
2014-08-05 14:39:09 +0200

    PMO-2838: integrated UploadValidationService

--- grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationService.groovy
+++ grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationService.groovy
@@ -1,12 +1,17 @@
 package org.pillarone.riskanalytics.core.upload
 
+import models.core.CoreModel
 import org.pillarone.riskanalytics.core.simulation.item.Simulation
 import org.pillarone.riskanalytics.core.simulation.item.SimulationProfile
 
 class UploadValidationService {
 
+    int counter = 0
+
     List<UploadValidationError> validate(Simulation simulation, SimulationProfile simulationProfile) {
-        //TODO: compare runtime parameters of sim and profile
-        return []
+        if (simulation.modelClass == CoreModel) {
+            return [new UploadValidationError(error: 'only application model ist allowed')]
+        }
+        []
     }
 }

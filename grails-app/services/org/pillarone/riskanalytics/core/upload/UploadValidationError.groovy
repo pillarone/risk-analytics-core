40afbcda861fc94924a46fe65c30fae39e8d5327
matthiasa <matthias.ansorge@googlemail.com>
2014-08-06 14:12:46 +0200

    PMO-2838: finished validation

--- grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationError.groovy
+++ grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationError.groovy
@@ -1,7 +1,17 @@
 package org.pillarone.riskanalytics.core.upload
 
 class UploadValidationError {
+    static enum REASON {
+        NO_PROFILE,
+        WRONG_RANDOM_SEED,
+        WRONG_NUMBER_OF_ITERATION,
+        WRONG_TEMPLATE,
+        WRONG_PARAMETER_COUNT,
+        WRONG_PARAMETER;
+    }
 
     String path
-    String error
+    REASON reason
+    String simulationParameterValue
+    String profileParameterValue
 }
--- grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationService.groovy
+++ grails-app/services/org/pillarone/riskanalytics/core/upload/UploadValidationService.groovy
@@ -5,9 +5,9 @@ import org.pillarone.riskanalytics.core.simulation.item.Simulation
 import org.pillarone.riskanalytics.core.simulation.item.SimulationProfile
 import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterHolder
 
-class UploadValidationService {
+import static org.pillarone.riskanalytics.core.upload.UploadValidationError.REASON.*
 
-    int counter = 0
+class UploadValidationService {
 
     List<UploadValidationError> validate(Simulation simulation, SimulationProfile simulationProfile) {
         Preconditions.checkNotNull(simulation)
@@ -27,41 +27,56 @@ class UploadValidationService {
         if (!simulation.loaded) {
             simulation.load()
         }
+        int simulationParameterCount = simulation.notDeletedParameterHolders.size()
+        int profileParameterCount = simulationProfile.notDeletedParameterHolders.size()
+        if (simulationParameterCount != profileParameterCount) {
+            errors.add(new UploadValidationError(
+                    reason: WRONG_PARAMETER_COUNT,
+                    simulationParameterValue: simulationParameterCount,
+                    profileParameterValue: profileParameterCount))
+            return
+        }
         simulation.notDeletedParameterHolders.each { ParameterHolder holder ->
             ParameterHolder profileHolder = simulationProfile.getParameterHolder(holder.path, 0)
-            if (!profileHolder) {
-                errors.add(new UploadValidationError(error: 'profi'))
-                return
-            }
-            if (holder.businessObject != profileHolder.businessObject) {
-                errors.add(new UploadValidationError(path: holder.path, error: 'busi'))
+            def simulationParameterValue = holder.businessObject
+            def simulationProfileParameterValue = profileHolder?.businessObject
+            if (simulationParameterValue != simulationProfileParameterValue) {
+                errors.add(new UploadValidationError(
+                        path: holder.path,
+                        reason: WRONG_PARAMETER,
+                        simulationParameterValue: simulationParameterValue,
+                        profileParameterValue: simulationProfileParameterValue))
             }
         }
     }
 
     private void validateTemplate(Simulation simulation, SimulationProfile simulationProfile, ArrayList<UploadValidationError> errors) {
         if (simulation.template != simulationProfile.template) {
-            errors.add(new UploadValidationError(error: 'temp'))
+            errors.add(new UploadValidationError(reason: WRONG_TEMPLATE))
         }
     }
 
     private void validateNumberOfIterations(Simulation simulation, SimulationProfile simulationProfile, ArrayList<UploadValidationError> errors) {
         if (simulation.numberOfIterations != simulationProfile.numberOfIterations) {
-            errors.add(new UploadValidationError(error: 'number'))
+            errors.add(new UploadValidationError(
+                    reason: WRONG_NUMBER_OF_ITERATION,
+                    simulationParameterValue: simulation.numberOfIterations,
+                    profileParameterValue: simulationProfile.numberOfIterations))
         }
     }
 
     private void validateRandomSeed(Simulation simulation, SimulationProfile simulationProfile, ArrayList<UploadValidationError> errors) {
         if (simulation.randomSeed != simulationProfile.randomSeed) {
-            errors.add(new UploadValidationError(error: 'rand'))
+            errors.add(new UploadValidationError(
+                    reason: WRONG_RANDOM_SEED,
+                    simulationParameterValue: simulation.randomSeed,
+                    profileParameterValue: simulationProfile.randomSeed))
         }
     }
 
     private void validateSimulationProfile(SimulationProfile simulationProfile, ArrayList<UploadValidationError> errors) {
         if (!simulationProfile) {
-            errors.add(new UploadValidationError(error: 'no simulation profile'))
+            errors.add(new UploadValidationError(reason: NO_PROFILE))
         }
     }
-
-
 }
